7a6834523955db4d5027ea0ee1436b095e1d617f
mvollebregt@xs4all.nl <mvollebregt@xs4all.nl>
2013-10-25 12:12:28 +0200

    Embedded jetty with wicket

---
+++ src/main/java/com/github/mvollebregt/todo/util/LocalServer.java
@@ -0,0 +1,38 @@
+package com.github.mvollebregt.todo.util;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+ 
+public class LocalServer {
+ 
+    public static void main(String[] args) throws Exception {
+
+    	Server server = new Server(8080);
+        
+        WebAppContext bb = new WebAppContext();
+        bb.setServer(server);
+        bb.setContextPath("/");
+        bb.setWar("src/main/webapp");
+
+        // START JMX SERVER
+        // MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        // MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
+        // server.getContainer().addEventListener(mBeanContainer);
+        // mBeanContainer.start();
+
+        server.setHandler(bb);
+
+        try {
+            System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
+            server.start();
+            System.in.read();
+            System.out.println(">>> STOPPING EMBEDDED JETTY SERVER");
+            server.stop();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+}

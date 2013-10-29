771c6b3f0ade924d62e149991cb13ae97759be5b
Michel Vollebregt <michel.vollebregt@spotdog.nl>
2013-10-29 11:37:47 +0100

    Added pagination

--- src/main/java/com/github/mvollebregt/todo/web/HomePage.java
+++ src/main/java/com/github/mvollebregt/todo/web/HomePage.java
@@ -2,13 +2,13 @@ package com.github.mvollebregt.todo.web;
 
 import java.util.List;
 
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.PageableListView;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.DataView;
-import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
 import com.github.mvollebregt.todo.model.Task;
 import com.github.mvollebregt.todo.services.TaskService;
@@ -24,13 +24,16 @@ public class HomePage extends WebPage {
 		
 		List<Task> list = taskService.list();
 		
-		add(new DataView<Task>("list", new ListDataProvider<Task>(list)) {
+		PageableListView<Task> listView = new PageableListView<Task>("list", list, 2) {
 			private static final long serialVersionUID = 3875748496308908368L;
 			@Override
-			protected void populateItem(Item<Task> item) {
+			protected void populateItem(ListItem<Task> item) {
 				Task task = item.getModelObject();
 				item.add(new Label("name", task.getDescription()));
 			}
-		});
+		};
+		
+		add(listView);
+		add(new PagingNavigator("navigator", listView));
     }
 }
